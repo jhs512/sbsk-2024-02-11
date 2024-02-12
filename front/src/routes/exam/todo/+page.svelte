cea91f5e40ef6b8a685896468ce5e4ce40d0f62d
jhs512 <jangka512@gmail.com>
2024-02-12 16:12:31 +0900

    work

--- front/src/routes/exam/todo/+page.svelte
+++ front/src/routes/exam/todo/+page.svelte
@@ -5,11 +5,13 @@
 		completed: boolean;
 	}
 
+	let editingTodo: Todo = $state({ id: 0, content: '', completed: false });
+
 	function makeTodo(id: number, content: string, completed: boolean): Todo {
 		return { id, content, completed };
 	}
 
-	const todos: Todo[] = $state([
+	let todos: Todo[] = $state([
 		makeTodo(1, '할일 1', false),
 		makeTodo(2, '할일 2', false),
 		makeTodo(3, '할일 3', false)
@@ -29,6 +31,26 @@
 		form.todoContent.value = '';
 		form.todoContent.focus();
 	}
+
+	function showTodoEditModal(todo: Todo) {
+		Object.assign(editingTodo, todo);
+
+		const modal = document.getElementById('todo-edit-modal') as HTMLDialogElement;
+		modal.showModal();
+
+		modal.querySelector('input')?.focus();
+	}
+
+	function submitEditTodoForm(this: HTMLFormElement, event: Event) {
+		const form = this;
+
+		todos = todos.map((todo) =>
+			todo.id == editingTodo.id ? { ...todo, content: editingTodo.content } : todo
+		);
+
+		const modal = document.getElementById('todo-edit-modal') as HTMLDialogElement;
+		modal.close();
+	}
 </script>
 
 <h1>할일</h1>
@@ -45,12 +67,49 @@
 			<li class="flex items-center gap-2">
 				{todo.content}
 				<input type="checkbox" class="checkbox" bind:checked={todo.completed} />
-				<button class="btn btn-outline btn-sm">수정</button>
+				<button class="btn btn-outline btn-sm" onclick={() => showTodoEditModal(todo)}>수정</button>
 				<button class="btn btn-outline btn-sm">삭제</button>
 			</li>
 		{/each}
 	</ul>
 {/snippet}
+
+<dialog id="todo-edit-modal" class="modal">
+	<div class="modal-box">
+		<form method="dialog">
+			<button class="btn btn-circle btn-ghost btn-sm absolute right-2 top-2">✕</button>
+		</form>
+		<form class="grid gap-3" on:submit|preventDefault={submitEditTodoForm}>
+			<label class="form-control">
+				<div class="label">
+					<span class="label-text">할일</span>
+				</div>
+				<input
+					type="text"
+					placeholder="할일을 입력해주세요."
+					class="input input-bordered"
+					bind:value={editingTodo.content}
+				/>
+			</label>
+			<div class="grid grid-cols-2 gap-3">
+				<button class="btn btn-primary">저장</button>
+				<button
+					type="button"
+					class="btn btn-outline"
+					onclick={() => {
+					const modal = document.getElementById('todo-edit-modal') as HTMLDialogElement;
+					modal.close();
+				}}
+					>취소</button
+				>
+			</div>
+		</form>
+	</div>
+
+	<form method="dialog" class="modal-backdrop">
+		<button></button>
+	</form>
+</dialog>
 
 {@render TodoList(todos)}
 <hr />
