0f8052d6f5d88b0d7560cd3b2dcac8d33e4d675e
jhs512 <jangka512@gmail.com>
2024-02-12 15:32:35 +0900

    work

--- front/src/routes/+layout.svelte
+++ front/src/routes/+layout.svelte
@@ -1,12 +1,9 @@
 <script lang="ts">
-	import rq from '$lib/rq/rq';
 	import '../app.pcss';
 
 	const { children } = $props();
 </script>
 
-<!-- svelte-ignore a11y-click-events-have-key-events -->
-<!-- svelte-ignore a11y-no-static-element-interactions -->
-<header onclick={() => rq.msgInfo('헤더 클릭됨')}>헤더</header>
+<header>헤더</header>
 <main class="flex flex-grow flex-col">{@render children()}</main>
 <footer>푸터</footer>
